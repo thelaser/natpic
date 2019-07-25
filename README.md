7f7f66d5aea884d85569fe402a8d387e2d99ef0d
Pau Garcia <pgv958@gmail.com>
2019-07-25 12:41:03 +0200

    Transferred

---
+++ README.md
@@ -0,0 +1,8 @@
+Hello everyone!
+
+This is a simple script in Perl, which downloads from National's Geographic website the picture of the day.
+
+If no parameter is given, saves the pics it downloads in ~/Pictures/NG, and if a specific folder is supplied, it saves it there.
+
+Beware, since the script works with the raw html from NG's website, if any change is performed to 
+the actual image link, the script will probably stop working. 
---
+++ natpic.pl
@@ -0,0 +1,72 @@
+#!/usr/local/bin/perl     
+use strict;
+use File::Path qw(make_path);
+
+# Script to download image of the day from National Geographic
+
+
+#Global var
+my $dirname = join "", $ENV{"HOME"}, "/Pictures/NG/";
+
+## Arguments treatement
+
+if (scalar @ARGV > 1) {
+    print "The program only accepts 1 argument, the destination folder";
+    exit;
+}
+elsif ($ARGV[0] eq "help") {
+    print "Only one option available, the destination folder, which is the first argument. If not provided, the output defaults to ~/Pictures/NG/";
+    exit;
+}
+elsif (scalar @ARGV == 1) {
+    $dirname = $ARGV[0];
+    if ((substr $dirname, -1) ne "/"){
+        $dirname = join "", $dirname, "/";
+    }
+    print "$dirname \n"; 
+}
+
+
+
+################ Subroutines ######################
+
+
+sub download_save {
+    # this variable holds the filename
+    #my @list = @_;
+    
+    my $NAME = join "", "NGPic_", `date +"%m-%d-%Y"|tr -d "\n"`, ".jpg";
+    my $final_dest = join "", $dirname, $NAME;
+
+
+    # we only want to download and save if it hasn't been done previously for the same day
+    if (!-e $final_dest) {
+        
+        # we dowload the pic from national geographic's web into temporal folder
+        `wget http://photography.nationalgeographic.com/photography/photo-of-the-day/ --quiet -O- > /tmp/index`;
+        
+        # here we extract the image link from NG's downloaded website
+        my $link=`grep -m 1 https://yourshot.nationalgeographic.com/u/.* /tmp/index -o`;
+        my $bettered_link = substr $link, 0, -5;
+        
+        # download and save image to desired folder (by default ~/Pictures/NG)
+        `wget $bettered_link --quiet -O $final_dest`;
+        `rm /tmp/index`;
+    }
+
+}
+
+################ Main ######################
+
+
+if (-d $dirname) {
+    download_save();
+}
+
+else {
+    ## if folder doesn't already exist, let's create it
+    eval { make_path($dirname) };
+    download_save();
+}
+
+#############################################
